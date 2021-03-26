6c8ca2cc614a5b03ac6b8dadfd0b7d8eceb9c9b6
svwolter <sven.wolter@iqb.hu-berlin.de>
2021-03-26 17:33:57 +0100

    Apply common code style rules

--- vo_code/DBConnectionAuthoring.php
+++ vo_code/DBConnectionAuthoring.php
@@ -6,17 +6,19 @@
 
 require_once('DBConnection.php');
 
-class DBConnectionAuthoring extends DBConnection {
-    
-    public function getUnitListByWorkspace($wsId) {
+class DBConnectionAuthoring extends DBConnection
+{
+
+    public function getUnitListByWorkspace($wsId)
+    {
         $myreturn = [];
         if (($this->pdoDBhandle != false) and ($wsId > 0)) {
             $sql = $this->pdoDBhandle->prepare(
                 'SELECT units.id, units.key, units.label FROM units
                     WHERE units.workspace_id =:ws
                     ORDER BY units.key');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':ws' => $wsId))) {
 
                 $data = $sql->fetchAll(PDO::FETCH_ASSOC);
@@ -31,7 +33,8 @@ class DBConnectionAuthoring extends DBConnection {
     // / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
     // returns all workspaces for the user associated with the given token
     // returns [] if token not valid or no workspaces 
-    public function getWorkspaceList($token) {
+    public function getWorkspaceList($token)
+    {
         $myreturn = [];
         if (($this->pdoDBhandle != false) and (strlen($token) > 0)) {
             $sql = $this->pdoDBhandle->prepare(
@@ -41,8 +44,8 @@ class DBConnectionAuthoring extends DBConnection {
                     INNER JOIN sessions ON  users.id = sessions.user_id
                     WHERE sessions.token =:token
                     ORDER BY workspaces.name');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':token' => $token))) {
 
                 $data = $sql->fetchAll(PDO::FETCH_ASSOC);
@@ -59,24 +62,25 @@ class DBConnectionAuthoring extends DBConnection {
     // returns the name of the workspace given by id
     // returns '' if not found
     // token is not refreshed
-    public function getWorkspaceName($workspace_id) {
+    public function getWorkspaceName($workspace_id)
+    {
         $myreturn = '';
         if ($this->pdoDBhandle != false) {
 
             $sql = $this->pdoDBhandle->prepare(
                 'SELECT workspaces.name FROM workspaces
                     WHERE workspaces.id=:workspace_id');
-                
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':workspace_id' => $workspace_id))) {
-                    
-                $data = $sql -> fetch(PDO::FETCH_ASSOC);
+
+                $data = $sql->fetch(PDO::FETCH_ASSOC);
                 if ($data != false) {
                     $myreturn = $data['name'];
                 }
             }
         }
-            
+
         return $myreturn;
     }
 
@@ -84,19 +88,20 @@ class DBConnectionAuthoring extends DBConnection {
     // adds new user if no user with the given name exists
     // returns true if ok, false if admin-token not valid or user already exists
     // token is refreshed via isSuperAdmin
-    public function addUnit($workspaceId, $key, $label, $sourceUnit) {
+    public function addUnit($workspaceId, $key, $label, $sourceUnit)
+    {
         $myreturn = false;
         if ($sourceUnit == 0) {
             $sql = $this->pdoDBhandle->prepare(
                 'INSERT INTO units (workspace_id, key, label, lastchanged) VALUES (:workspace, :key, :label, :now)');
-                
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':workspace' => $workspaceId,
                 ':key' => $key,
                 ':label' => $label,
                 ':now' => date('Y-m-d G:i:s', time())
-                ))) {
-                    
+            ))) {
+
                 $myreturn = true;
             }
         } else {
@@ -104,18 +109,18 @@ class DBConnectionAuthoring extends DBConnection {
             $sql = $this->pdoDBhandle->prepare(
                 'SELECT * FROM units
                     WHERE units.id =:id and units.workspace_id=:ws');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':ws' => $workspaceId,
                 ':id' => $sourceUnit))) {
 
-                $data = $sql -> fetch(PDO::FETCH_ASSOC);
+                $data = $sql->fetch(PDO::FETCH_ASSOC);
                 if ($data != false) {
                     $sql = $this->pdoDBhandle->prepare(
                         'INSERT INTO units (workspace_id, key, label, lastchanged, description, def, authoringtool_id, player_id, defref) 
                             VALUES (:workspace, :key, :label, :now, :description, :def, :atId, :pId, :defref)');
-                        
-                    if ($sql -> execute(array(
+
+                    if ($sql->execute(array(
                         ':workspace' => $workspaceId,
                         ':key' => $key,
                         ':label' => $label,
@@ -125,67 +130,70 @@ class DBConnectionAuthoring extends DBConnection {
                         ':atId' => $data['authoringtool_id'],
                         ':pId' => $data['player_id'],
                         ':defref' => $data['defref']
-                        ))) {
-                            
+                    ))) {
+
                         $myreturn = true;
                     }
                 }
             }
 
         }
-            
+
         return $myreturn;
     }
 
-    public function deleteUnits($workspaceId, $unitIds) {
+    public function deleteUnits($workspaceId, $unitIds)
+    {
         $myreturn = false;
         $sql = $this->pdoDBhandle->prepare(
             'DELETE FROM units
                 WHERE units.workspace_id = :ws and units.id in (' . implode(',', $unitIds) . ')');
-            
-        if ($sql -> execute(array(
+
+        if ($sql->execute(array(
             ':ws' => $workspaceId))) {
-                
+
             $myreturn = true;
         }
-            
+
         return $myreturn;
     }
 
-    public function moveUnits($workspaceId, $unitIds, $targetWorkspace) {
+    public function moveUnits($workspaceId, $unitIds, $targetWorkspace)
+    {
         $update_count = 0;
-        foreach($unitIds as $uid) {
+        foreach ($unitIds as $uid) {
             $sql_update = $this->pdoDBhandle->prepare(
                 'UPDATE units
                     SET workspace_id =:ws
                     WHERE id =:id');
-    
+
             if ($sql_update != false) {
                 if ($sql_update->execute(array(
                     ':ws' => $targetWorkspace,
                     ':id' => $uid))) {
-                        $update_count += 1;
+                    $update_count += 1;
                 }
             }
         }
-           
+
         return $update_count === count($unitIds);
     }
 
     // filename will be unit key plus extension '.xml'; but if def is big there will be a second file
     // with extension '.voud'
-    public function writeUnitDefToZipFile($wsId, $unitId, $targetZip, $wsName) {
+    public function writeUnitDefToZipFile($wsId, $unitId, $targetZip, $wsName)
+    {
         $myreturn = false;
         if (($this->pdoDBhandle != false) and ($wsId > 0)) {
             $sql = $this->pdoDBhandle->prepare(
                 'SELECT units.key, units.label, units.def, units.player_id, units.lastchanged FROM units
                     WHERE units.id =:id and units.workspace_id=:ws');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':ws' => $wsId,
                 ':id' => $unitId))) {
 
-                $data = $sql -> fetch(PDO::FETCH_ASSOC);
+                $data = $sql->fetch(PDO::FETCH_ASSOC);
                 if ($data != false) {
                     $xRootElement = new SimpleXMLElement(
                         '<Unit xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation=' .
@@ -204,7 +212,7 @@ class DBConnectionAuthoring extends DBConnection {
                         $xDefElement = $xRootElement->addChild('Definition', $data['def']);
                         $xDefElement->addAttribute('player', $data['player_id']);
                     }
-                    
+
                     $xfile = dom_import_simplexml($xRootElement)->ownerDocument;
                     $xfile->formatOutput = true;
                     $targetZip->addFromString($data['key'] . '.xml', $xfile->saveXML());
@@ -216,7 +224,8 @@ class DBConnectionAuthoring extends DBConnection {
         return $myreturn;
     }
 
-    public function getUnitProperties($wsId, $unitId) {
+    public function getUnitProperties($wsId, $unitId)
+    {
         $myreturn = [];
         if (($this->pdoDBhandle != false) and ($wsId > 0)) {
             $sql = $this->pdoDBhandle->prepare(
@@ -224,16 +233,16 @@ class DBConnectionAuthoring extends DBConnection {
                     units.lastchanged, units.authoringtool_id as authoringtoolId,
                     units.player_id as playerId FROM units
                     WHERE units.id =:id and units.workspace_id=:ws');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':ws' => $wsId,
                 ':id' => $unitId))) {
 
-                $data = $sql -> fetch(PDO::FETCH_ASSOC);
+                $data = $sql->fetch(PDO::FETCH_ASSOC);
                 if ($data != false) {
                     $myreturn = $data;
                     setlocale(LC_TIME, "de_DE");
-                    $myreturn['lastchangedStr'] = strftime("%d.%m.%Y %H:%M",strtotime($data['lastchanged']));
+                    $myreturn['lastchangedStr'] = strftime("%d.%m.%Y %H:%M", strtotime($data['lastchanged']));
                     // $myreturn['lastchangedStr'] = strftime('%x', $data['lastchanged']);
                 }
             }
@@ -241,18 +250,19 @@ class DBConnectionAuthoring extends DBConnection {
         return $myreturn;
     }
 
-    public function getUnitDesignData($wsId, $unitId) {
+    public function getUnitDesignData($wsId, $unitId)
+    {
         $myreturn = [];
         if (($this->pdoDBhandle != false) and ($wsId > 0)) {
             $sql = $this->pdoDBhandle->prepare(
                 'SELECT units.id, units.key, units.label, units.def, units.authoringtool_id, units.player_id FROM units
                     WHERE units.id =:id and units.workspace_id=:ws');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':ws' => $wsId,
                 ':id' => $unitId))) {
 
-                $data = $sql -> fetch(PDO::FETCH_ASSOC);
+                $data = $sql->fetch(PDO::FETCH_ASSOC);
                 if ($data != false) {
                     $myreturn = $data;
                 }
@@ -261,18 +271,19 @@ class DBConnectionAuthoring extends DBConnection {
         return $myreturn;
     }
 
-    public function getUnitPreviewData($wsId, $unitId) {
+    public function getUnitPreviewData($wsId, $unitId)
+    {
         $myreturn = [];
         if (($this->pdoDBhandle != false) and ($wsId > 0)) {
             $sql = $this->pdoDBhandle->prepare(
                 'SELECT units.id, units.key, units.label, units.def, units.player_id FROM units
                     WHERE units.id =:id and units.workspace_id=:ws');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':ws' => $wsId,
                 ':id' => $unitId))) {
 
-                $data = $sql -> fetch(PDO::FETCH_ASSOC);
+                $data = $sql->fetch(PDO::FETCH_ASSOC);
                 if ($data != false) {
                     $myreturn = $data;
                 }
@@ -281,17 +292,18 @@ class DBConnectionAuthoring extends DBConnection {
         return $myreturn;
     }
 
-    public function getUnitAuthoringTool($unitId) {
+    public function getUnitAuthoringTool($unitId)
+    {
         $myreturn = '';
         if (($this->pdoDBhandle != false) and ($unitId > 0)) {
             $sql = $this->pdoDBhandle->prepare(
                 'SELECT units.authoringtool_id FROM units
                     WHERE units.id =:id');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':id' => $unitId))) {
 
-                $data = $sql -> fetch(PDO::FETCH_ASSOC);
+                $data = $sql->fetch(PDO::FETCH_ASSOC);
                 if ($data != false) {
                     $myreturn = $data['authoringtool_id'];
                 }
@@ -300,17 +312,18 @@ class DBConnectionAuthoring extends DBConnection {
         return $myreturn;
     }
 
-    public function getUnitItemPlayerId($unitId) {
+    public function getUnitItemPlayerId($unitId)
+    {
         $myreturn = '';
         if (($this->pdoDBhandle != false) and ($unitId > 0)) {
             $sql = $this->pdoDBhandle->prepare(
                 'SELECT units.player_id FROM units
                     WHERE units.id =:id');
-        
-            if ($sql -> execute(array(
+
+            if ($sql->execute(array(
                 ':id' => $unitId))) {
 
-                $data = $sql -> fetch(PDO::FETCH_ASSOC);
+                $data = $sql->fetch(PDO::FETCH_ASSOC);
                 if ($data != false) {
                     $myreturn = $data['player_id'];
                 }
@@ -319,7 +332,8 @@ class DBConnectionAuthoring extends DBConnection {
         return $myreturn;
     }
 
-    public function changeUnitProps($myId, $myKey, $myLabel, $myDescription) {
+    public function changeUnitProps($myId, $myKey, $myLabel, $myDescription)
+    {
         $myreturn = false;
         $sql_update = $this->pdoDBhandle->prepare(
             'UPDATE units
@@ -330,15 +344,16 @@ class DBConnectionAuthoring extends DBConnection {
             $myreturn = $sql_update->execute(array(
                 ':id' => $myId,
                 ':key' => $myKey,
-                ':label'=> $myLabel,
-                ':description'=>$myDescription,
+                ':label' => $myLabel,
+                ':description' => $myDescription,
                 ':now' => date('Y-m-d G:i:s', time())
             ));
         }
         return $myreturn;
     }
 
-    public function setUnitAuthoringTool($myId, $myTool) {
+    public function setUnitAuthoringTool($myId, $myTool)
+    {
         $myreturn = false;
         $sql_update = $this->pdoDBhandle->prepare(
             'UPDATE units
@@ -356,7 +371,8 @@ class DBConnectionAuthoring extends DBConnection {
         return $myreturn;
     }
 
-    public function setUnitDefinition($myId, $myUnitdef, $playerId) {
+    public function setUnitDefinition($myId, $myUnitdef, $playerId)
+    {
         $myreturn = false;
         $sql_update = $this->pdoDBhandle->prepare(
             'UPDATE units
@@ -374,4 +390,5 @@ class DBConnectionAuthoring extends DBConnection {
         return $myreturn;
     }
 }
+
 ?>
